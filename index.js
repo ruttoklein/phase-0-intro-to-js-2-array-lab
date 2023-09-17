9519cd2e26d352ebfb62e6b5b64eeb0efcb41b55
ruttklein <klein.rutto@student.moringaschool.com>
2023-09-17 23:41:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution here!
+// index.js
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Now you can use the 'cats' array in your code
+// index.js
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Export the 'cats' array
+module.exports = { cats };
+
+// indexTest.js
+const { cats } = require('./index'); // Import the 'cats' array from index.js
+
+// Now you can use the 'cats' array in your tests
